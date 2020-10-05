4e4525cfa1e6949a29a48a5e42c6b531ee2cd40b
eobeans <516225899@qq.com>
2020-10-05 17:06:06 +0800

    feat：新增查询表单和列表
    调整APP代码

--- src/App.js
+++ src/App.js
@@ -9,46 +9,52 @@ import { UploadOutlined, UserOutlined, VideoCameraOutlined } from '@ant-design/i
 
 const { Header, Content, Footer } = Layout;
 
-function App(props) {
-  return (
-    <div>
-      <Layout className="layout">
-        <Header>
-          <div className="logo" />
-          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
-            <Menu.Item key="1" icon={<UserOutlined />}>
-              <NavLink to="/ywkclb">
-                药物库存列表
-              </NavLink>
-            </Menu.Item>
-            <Menu.Item key="2" icon={<VideoCameraOutlined />}>
-              <NavLink to="/login">
-                登入页面
-              </NavLink>
-            </Menu.Item>
-            <Menu.Item key="3" icon={<UploadOutlined />}>
-              <NavLink to="/">
-                返回
-              </NavLink>
-            </Menu.Item>
-          </Menu>
-        </Header>
-        <Content style={{ padding: '0 50px' }}>
-          <div className="site-layout-content">
-            <Switch>
-              {routes.map((route, index) => (
-                <Route key = {index}  {...route} />
-              ))}
-            </Switch>
-          </div>
-        </Content>
-        <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
-      </Layout>
-
-      {props.children}
-      
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+  render() {
+    return (
+      <div className="jtyw-main-container">
+        <Layout className="layout">
+          <Header>
+            <div className="logo" />
+            <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
+              <Menu.Item key="1" icon={<UserOutlined />}>
+                <NavLink to="/ywkclb">
+                  药物库存列表
+                </NavLink>
+              </Menu.Item>
+              <Menu.Item key="2" icon={<VideoCameraOutlined />}>
+                <NavLink to="/login">
+                  登入页面
+                </NavLink>
+              </Menu.Item>
+              <Menu.Item key="3" icon={<UploadOutlined />}>
+                <NavLink to="/">
+                  返回
+                </NavLink>
+              </Menu.Item>
+            </Menu>
+          </Header>
+          <Content style={{ padding: '0 50px' }}>
+            <div className="site-layout-content">
+              <Switch>
+                {routes.map((route, index) => (
+                  <Route key = {index}  {...route} />
+                ))}
+              </Switch>
+            </div>
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>家庭药物管理系统 ©2020 Created by Eobeans</Footer>
+        </Layout>
+  
+        {this.props.children}
+        
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/ywkclb/index.js
+++ src/ywkclb/index.js
@@ -1,9 +1,91 @@
 import React from 'react';
+import { Row, Col, Button, Form, Input, Space, Pagination, Table } from 'antd';
+import { SearchOutlined, UndoOutlined } from '@ant-design/icons';
+import './index.css';
 
-function ywkclb (props) {
+function ywkclb () {
+  let total = 50;
+  let onFinish = (formData) => {
+    console.log("onFinish!", formData);
+  }
+  let onFinishFailed = (err) => {
+    console.log(err)
+  }
+  let onShowSizeChange = (current, size) => {
+    console.log('current', current);
+  }
+  let onChange = (pageNumber) => {
+    console.log('Page: ', pageNumber);
+  }
+  const columns = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      width: 150,
+    },
+    {
+      title: 'Age',
+      dataIndex: 'age',
+      width: 150,
+    },
+    {
+      title: 'Address',
+      dataIndex: 'address',
+    },
+  ];
+  
+  const data = [];
+  for (let i = 0; i < 100; i++) {
+    data.push({
+      key: i,
+      name: `Edward King ${i}`,
+      age: 32,
+      address: `London, Park Lane no. ${i}`,
+    });
+  }
   return (
-    <div id='ywkclb'>
-      药物库存列表查询页面
+    <div id='ywkclb' className="jtyw-layout">
+      <div className="jtyw-title">药物库存列表查询页面</div>
+      <div className="jtyw-btn-list">
+        <Space>
+          <Button type="primary" size="small" icon={<SearchOutlined />}>查询</Button>
+          <Button type="primary" size="small" icon={<UndoOutlined />}>reset</Button>
+        </Space>
+      </div>
+      <div className="jtyw-search-form">
+        <Form
+          name="basic"
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          size="small"
+        >
+          <Row>
+            <Col span="8">
+              <span className="jtyw-search-label">患者姓名</span>
+              <Input className="jtyw-search-item"></Input>
+            </Col>
+            <Col span="8">
+              <span className="jtyw-search-label">药物名称</span>
+              <Input className="jtyw-search-item"></Input>
+            </Col>
+            <Col span="8">
+              <span className="jtyw-search-label">药物名称</span>
+              <Input className="jtyw-search-item"></Input>
+            </Col>
+          </Row>
+        </Form>
+      </div>
+      <div className="jtyw-table-list">
+        <Table columns={columns} dataSource={data} loading={false} bordered size="small"
+        pagination={false} scroll={{ y: 654 }} />
+      </div>
+      <div className="jtyw-footer">
+        <Pagination size="small" total={total} defaultCurrent={1}
+          showSizeChanger showQuickJumper
+          defaultPageSize={10} pageSizeOptions={[10, 20, 50, 100]}
+          onShowSizeChange={onShowSizeChange} onChange={onChange} />
+      </div>
     </div>  
   );
 }
